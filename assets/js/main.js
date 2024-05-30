dbc554c5ceb46d3d4f45b91a422d0be6a0e45a9e
Criss-TeC <cristiantelloc@gmail.com>
2024-05-29 22:30:40 -0400

    feat: end crud

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,9 +26,11 @@ const addTask = (e) => {
 
     tasklist.push(task);
     // alert('La tarea fe agregada con exito.');
-
+    document.getElementById("task_name").value = "";
+    document.getElementById("task_description").value = "";
     console.log(tasklist);
     showTasks();
+
 }
 
 const showTasks = () => {
@@ -39,9 +41,11 @@ const showTasks = () => {
         row.innerHTML = `
         <td>${task.name}</td>
         <td>${task.description}</td>
+        <td>${task.completed}</td>
         <td>
             <a href = "#" onclick = "deleteTask(${index})">eliminar</a> |
-            <a href="#" onclick="editTask(${index})">editar</a>
+            <a href="#" onclick="editTask(${index})">editar</a> |
+            <a href="#" onclick="completeTask(${index})">completar</a>
         </td>
         `;
         tableTaskBody.appendChild(row);
@@ -69,23 +73,55 @@ const editTask = (index) => {
     let addTaskButton = document.getElementById("add_task_button");
     addTaskButton.setAttribute("disabled","");
     addTaskButton.setAttribute("class",DISABLE_BUTTON_STYLE)
+    
     let updateTaskButton = document.getElementById("update_task_button");
     updateTaskButton.removeAttribute("disabled");
-    updateTaskButton.setAttribute("class",ENABLE_BUTTON_STYLE)
+    updateTaskButton.setAttribute("class",ENABLE_BUTTON_STYLE);
     // Asignar los valores a los cuadros de texto
+    updateTaskButton.onclick = (e) => updateTask(e, index)
+    
     let nameText = document.getElementById("task_name");
     let descriptionText = document.getElementById("task_description");
 
-    const task = tasklist[index]
+    const task = tasklist[index];
     nameText.value = task.name;
     descriptionText.value = task.description;
-
-    updateTaskButton.addEventListener(updateTask(Event, index));
 }
 
-const updateTask = (index)=>{
-
-    debugger;
+const updateTask = (e, index)=>{
+    e.preventDefault();
+    console.log(e, index);
+    
     // tomar los valores de los cuadros de texto y actualizarlos en la lista
+    let nameText = document.getElementById("task_name");
+    let descriptionText = document.getElementById("task_description");
+    
+    tasklist[index].name = nameText.value;
+    tasklist[index].description = descriptionText.value;
+
+    showTasks();
+    
     // cambiar los estilos y de los botones
+    let addTaskButton = document.getElementById("add_task_button");
+    addTaskButton.setAttribute("disabled","");
+    addTaskButton.setAttribute("class",ENABLE_BUTTON_STYLE)
+    
+    let updateTaskButton = document.getElementById("update_task_button");
+    updateTaskButton.removeAttribute("disabled");
+    updateTaskButton.setAttribute("class",DISABLE_BUTTON_STYLE);
+
+    document.getElementById("task_name").value = "";
+    document.getElementById("task_description").value = "";
+
+    updateTaskButton.onclick = null;
 }
+
+const completeTask = (index) =>{
+    tasklist[index].toggleCompetion();
+    showTasks();
+    
+    let cardCompletedTasks = document.getElementById("card_completed_tasks");
+    const completedTasks = tasklist.filter((task) => task.completed);
+    cardCompletedTasks.innerHTML = `Total de tareas completadas (${completedTasks.length})`;
+}
+
